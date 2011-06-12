c6fb222ee5d7be5254648c8bc6f031f03d40bddb
Daniel Lucraft <dan@fluentradical.com>
2011-06-12 08:29:18 +0100

    Swt.jar_path should return the absolute path

--- lib/swt/jar.rb
+++ lib/swt/jar.rb
@@ -2,28 +2,31 @@ require 'rbconfig'
 
 module Swt
   def self.jar_path
+    File.expand_path(relative_jar_path, __FILE__)
+  end
+  
+  def self.relative_jar_path
     case Config::CONFIG["host_os"]
     when /darwin/i
       if Config::CONFIG["host_cpu"] == "x86_64"
-        '../vendor/swt_osx64'
+        '../../../vendor/swt_osx64'
       else
-        '../vendor/swt_osx'
+        '../../../vendor/swt_osx'
       end
     when /linux/i
       if %w(amd64 x84_64).include? Config::CONFIG["host_cpu"]
-        '../vendor/swt_linux64'
+        '../../../vendor/swt_linux64'
       else
-        '../vendor/swt_linux'
+        '../../../vendor/swt_linux'
       end
     when /windows|mswin/i
-      '../vendor/swt_win32'
+      '../../../vendor/swt_win32'
     end
   end
 
-  path = File.expand_path(File.dirname(__FILE__) + "/../" + Swt.jar_path)
-  if File.exist?(path + ".jar")
-    require path
+  if File.exist?(jar_path + ".jar")
+    require jar_path
   else
-    raise "SWT jar file required: #{path}.jar"
+    raise "swt jar file required: #{jar_path}.jar"
   end
 end
