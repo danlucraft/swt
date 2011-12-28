06a2d0ff46b51d638911588ccc5239225da388cf
Daniel Lucraft <dan@fluentradical.com>
2011-12-28 14:00:25 +0000

    Bump to version 0.12

--- lib/swt/minimal.rb
+++ lib/swt/minimal.rb
@@ -4,7 +4,7 @@ require 'swt/event_loop'
 require 'swt/cucumber_runner'
 
 module Swt
-  VERSION = "0.11" # also change in swt.gemspec
+  VERSION = "0.12" # also change in swt.gemspec
   
   import org.eclipse.swt.SWT
   
