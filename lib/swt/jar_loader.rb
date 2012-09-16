a20c3491ad01d0ebc65e9a08bf8ed6ef09327ac0
Eric Watson <wasnotrice@gmail.com>
2012-09-16 09:50:33 -0500

    Correct x84_64 -> x86_64

--- lib/swt/jar_loader.rb
+++ lib/swt/jar_loader.rb
@@ -8,19 +8,19 @@ module Swt
   def self.relative_jar_path
     case Config::CONFIG["host_os"]
     when /darwin/i
-      if %w(amd64 x84_64).include? Config::CONFIG["host_cpu"]
+      if %w(amd64 x86_64).include? Config::CONFIG["host_cpu"]
         '../../../vendor/swt/swt-osx64'
       else
         '../../../vendor/swt/swt-osx32'
       end
     when /linux/i
-      if %w(amd64 x84_64).include? Config::CONFIG["host_cpu"]
+      if %w(amd64 x86_64).include? Config::CONFIG["host_cpu"]
         '../../../vendor/swt/swt-linux64'
       else
         '../../../vendor/swt/swt-linux32'
       end
     when /windows|mswin/i
-      if %w(amd64 x84_64).include? Config::CONFIG["host_cpu"]
+      if %w(amd64 x86_64).include? Config::CONFIG["host_cpu"]
         '../../../vendor/swt/swt-win64'
       else
         '../../../vendor/swt/swt-win32'
