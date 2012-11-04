765c274a62c1c28ae31841f32b9a5db58ac0fee2
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2012-11-04 20:38:47 +0100

    fixing cpu detection

--- lib/swt/jar_loader.rb
+++ lib/swt/jar_loader.rb
@@ -1,31 +1,38 @@
 require 'rbconfig'
 
 module Swt
+
+  X64_BIT_CPUS = %w(amd64 x84_64 x86_64)
+
   def self.jar_path
     File.expand_path(relative_jar_path, __FILE__)
   end
-  
+
   def self.relative_jar_path
     case Config::CONFIG["host_os"]
     when /darwin/i
-      if %w(amd64 x86_64).include? Config::CONFIG["host_cpu"]
+      if x64_bit_cpu?
         '../../../vendor/swt/swt-osx64'
       else
         '../../../vendor/swt/swt-osx32'
       end
     when /linux/i
-      if %w(amd64 x86_64).include? Config::CONFIG["host_cpu"]
+      if x64_bit_cpu?
         '../../../vendor/swt/swt-linux64'
       else
         '../../../vendor/swt/swt-linux32'
       end
     when /windows|mswin/i
-      if %w(amd64 x86_64).include? Config::CONFIG["host_cpu"]
+      if x64_bit_cpu?
         '../../../vendor/swt/swt-win64'
       else
         '../../../vendor/swt/swt-win32'
       end
     end
+  end
+
+  def self.x64_bit_cpu?
+    X64_BIT_CPUS.include? Config::CONFIG["host_cpu"]
   end
 
   if File.exist?(jar_path + ".jar")
